f19cfc66024512104e0d49f7ccabf9d0cb0012df
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2013-12-22 00:06:30 +0100

    xrootd build file made simpler, and bugfixed (cmake must build outside of xrootd src dif)

--- eups/declare.sh
+++ eups/declare.sh
@@ -9,27 +9,31 @@ source functions.sh
 eups_dist mysql 5.1.61
 eups_dist xrootd qs5
 
-# lua deps
+# lua
 eups_dist lua 5.1.4
 eups_dist luasocket 2.0.2 
 eups_dist expat 2.0.1 
 eups_dist luaexpat 1.1
 eups_dist luaxmlrpc v1.2.1-2
 
-# python deps
+# python
 eups_dist virtualenv 1.7
 #eups_dist python 2.6.6
 eups_dist python virtualenv 
 eups_dist protobuf 2.4.1
 eups_dist mysqlpython 1.2.3
-eups_dist libevent 2.0.16-stable
 
-# twisted deps
+# twisted
 eups_dist zopeinterface 3.8.0
 eups_dist twisted 12.0.0
 
+# mysql-proxy
+eups_dist libevent 2.0.16-stable
+eups_dist mysqlproxy 0.8.2
+
+# qserv
 eups_dist qserv 533d661
-# test this on remote machine
+# TODO test this on remote machine
 
 # for test purpose only
 # undeclare everything
