b5df983c29c0def10112d1aa6531049d537d3de8
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2013-12-21 02:14:57 +0100

    product_dir/ups/ is now filled with EUPS_PKGROOT build/table files

--- eups/startup.py
+++ eups/startup.py
@@ -19,7 +19,7 @@ eups.commandCallbacks.add(cmdHook)
 # and then later:
 # qserv_ups @PRODUCT@ @VERSION@ <INSTALL-DIR> [GIT-HASH]
 hooks.config.distrib["builder"]["variables"]["QSERV UPS"] = """
-# copy remote ups directory in installdir
+# copy remote ups directory in product_dir
 # after having expanded build file
 # NOTE : *.build and .table file could be also retrieved from $EUPS_PKGROOT
 # but paths would then be harder to deduce
@@ -30,26 +30,26 @@ qserv_ups() {
     # would it be possible to get the real EUPS_PKGROOT build files instead of re-generating them ?
     gitrepo=${QSERV_BUILD_REPO}
 
-    if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
-        echo "lsst_ups requires at least three arguments"
+    if [ -z "$1" -o -z "$2" ]; then
+        echo "lsst_ups requires at least two arguments"
         exit 1
     fi
     productname=$1
     versionname=$2
-    installdir=$3
-    githash=$4
+    githash=$3
     if [ -z "$githash" ]; then
         githash="HEAD"
     fi
     currentdir=$(pwd)
     eups_files="eups/pkg/${productname}/${versionname}/ups"
-    mkdir -p $installdir/ups &&
-    git archive --verbose --format=tar --remote=$gitrepo --prefix=tmp/ ${githash} ${eups_files} | tar --extract --verbose --directory $installdir &&
-    mv $installdir/tmp/$eups_files/* $installdir/ups/ &&
-    rm -r $installdir/tmp &&
-    echo "eups expandbuild -i ${installdir}/ups/${productname}.build -V $versionname "
-    eups expandbuild -i ${installdir}/ups/${productname}.build -V $versionname 
-    git archive --verbose --format=tar --remote=$gitrepo --prefix=ups/ ${githash} ${productname} | tar --extract --verbose --directory $installdir || echo "No additional files required: ignore error"
+    mkdir -p ${product_dir}/ups &&
+    buildfile=${productname}-${versionname}.build &&
+    echo "Downloading ${pkgroot_url}/builds/${buildfile}" &&  
+    curl -L ${pkgroot_url}/builds/${buildfile} > ${product_dir}/ups/${buildfile} &&
+    tablefile=${productname}-${versionname}.table && 
+    echo "Downloading ${pkgroot_url}/tables/${tablefile}" &&
+    curl -L ${pkgroot_url}/tables/${tablefile} > ${product_dir}/ups/${tablefile}
+    
 }
 """
 
@@ -67,9 +67,7 @@ qserv_prepare() {
 
     #url=http://www.slac.stanford.edu/exp/lsst/qserv/download/current
 
-    pkgroot_url=${EUPS_PKGROOT}/tarballs
-    
-    if [ -d ${EUPS_PKGROOT}/tarballs ]; then
+    if [ -d "${EUPS_PKGROOT}" ]; then
       # Hack to make curl work with a local repository, 
       # note that :
       # 1. {EUPS_PKGROOT} consistency should have already been checked by eups
@@ -77,9 +75,11 @@ qserv_prepare() {
       # 2. eups doesn't support EUPS_PKGROOT starting with file:// protocol because : 
       #    - LocalTransporter doesn't support this protocol 
       #    - It couldn't be added to WebTransporter because urllib2.urlopen() fail while listing a directory with this protocol
-      pkgroot_url="file://"+${url}
+      pkgroot_url="file://${EUPS_PKGROOT}"
+    else
+      pkgroot_url=${EUPS_PKGROOT}
     fi
-    tarballs_url=${pkgroot_url}/tarballs    
+    tarballs_url=${pkgroot_url}/tarballs
 
     productname=$1
     versionname=$2
@@ -114,6 +114,7 @@ qserv_prepare() {
 
     # empty install dir if needed
     rm -rf * &&
+    echo "Downloading ${tarballs_url}/${archivename}"
     curl -L ${tarballs_url}/${archivename} > ${archivename} &&
     mkdir ${extractname} && 
     tar xf ${archivename} -C ${extractname} --strip-components 1 &&
