3ae861e4801b001edec62b231ad710c4334ae9f0
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-12-06 01:27:52 +0100

    Update qserv test script for git bisect

--- qserv_quick_and_dirty/qserv-test-branch.sh
+++ qserv_quick_and_dirty/qserv-test-branch.sh
@@ -1,8 +1,10 @@
 killall mysqld mysql-proxy xrootd java python
+QSERV_SRC_DIR=$HOME/src/qserv
+cd ${QSERV_SRC_DIR} &&
 setup -k -r . &&
 rm -rf build lib proxy bin cfg  &&
 eupspkg -e PREFIX=$PWD install &&
 qserv-configure.py --all --force &&
-~/qserv-run/2014_11.0/bin/qserv-start.sh &&
+~/qserv-run/2014_12/bin/qserv-start.sh &&
 qserv-test-integration.py &&
-~/qserv-run/2014_11.0/bin/qserv-stop.sh 
+~/qserv-run/2014_12/bin/qserv-stop.sh 
