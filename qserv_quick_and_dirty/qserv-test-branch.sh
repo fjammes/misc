068cd7f832d68d2a282fac400688b15019511bf8
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2014-12-06 01:53:08 +0100

    Improve git bisect script for Qserv

--- qserv_quick_and_dirty/qserv-test-branch.sh
+++ qserv_quick_and_dirty/qserv-test-branch.sh
@@ -1,10 +1,34 @@
-killall mysqld mysql-proxy xrootd java python
-QSERV_SRC_DIR=$HOME/src/qserv
-cd ${QSERV_SRC_DIR} &&
-setup -k -r . &&
-rm -rf build lib proxy bin cfg  &&
-eupspkg -e PREFIX=$PWD install &&
-qserv-configure.py --all --force &&
-~/qserv-run/2014_12/bin/qserv-start.sh &&
-qserv-test-integration.py &&
-~/qserv-run/2014_12/bin/qserv-stop.sh 
+#!/bin/bash
+# Pre-requisite:
+#   source loadLSST.bash
+#   setup qserv_distrib -t qserv
+#   setup -k -r ${QSERV_SRC_DIR}
+set -x
+set -e
+
+QSERV_RUN_DIR=$HOME/qserv-run/bisect
+
+# load eups "setup" function
+if [ -r "${EUPS_DIR}" -a -x "${EUPS_DIR}" ]; then
+    . "${EUPS_DIR}/bin/setups.sh"
+else
+    printf "ERROR: ${EUPS_DIR} must point on a readable directory"
+    exit 1
+fi
+
+if [ ! -r "${QSERV_DIR}/SConstruct" ]; then
+    printf "ERROR: ${QSERV_DIR} must be a source directory"
+    exit 1
+fi
+
+# cleaning, if source directory
+rm -r build lib proxy bin cfg
+killall mysqld mysql-proxy xrootd java python || printf "Unable to kill some services"
+
+# "scons install" doesn't use all proc
+eupspkg -e PREFIX=${QSERV_DIR} install
+qserv-configure.py --all --force -R ${QSERV_RUN_DIR}
+${QSERV_RUN_DIR}/bin/qserv-start.sh
+#qserv-test-integration.py
+qserv-check-integration.py --case=01 --load
+${QSERV_RUN_DIR}/bin/qserv-stop.sh
