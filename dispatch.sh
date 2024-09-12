a8f38c5df00cf1981f381c07628488fe2cda57d4
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-09-12 10:36:29 +0200

    Update current feature

--- dispatch.sh
+++ dispatch.sh
@@ -2,10 +2,28 @@
 
 set -euxo pipefail
 
-curl -L \
+url="https://api.github.com/repos/fjammes/misc/dispatches"
+
+token=$1
+
+build=true
+e2e=true
+push=true
+cluster_name="kind"
+
+payload="{\"build\": $build,\"e2e\": $e2e,\"push\": $push, \"cluster_name\": \"$cluster_name\"}"
+echo "Payload: $payload"
+
+if [ -z "$token" ]; then
+  echo "No token provided, skipping GitHub dispatch"
+else
+  echo "Dispatching event to GitHub"
+  curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $TOKEN" \
+  -H "Authorization: Bearer $token" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/fjammes/misc/dispatches \
-  -d '{"event_type":"e2e-science","client_payload":{"build":true,"e2e":false}}'
+  $url \
+  -d "{\"event_type\":\"e2e-science\",\"client_payload\":$payload}" || echo "ERROR Failed to dispatch event" >&2
+fi
+
